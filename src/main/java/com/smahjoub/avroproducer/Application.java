ddf6f6a2b93cd960dc7a402cc155fe2bcde6ad60
Saif Eddine <semahjoub@finaxys.com>
2019-07-08 11:47:42 +0200

    Add and configure google Guice for dependency injection

--- src/main/java/com/smahjoub/avroproducer/Application.java
+++ src/main/java/com/smahjoub/avroproducer/Application.java
@@ -1,9 +1,21 @@
 package com.smahjoub.avroproducer;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.smahjoub.avroproducer.injection.AvroProducerModule;
+import org.apache.log4j.Logger;
+
 public class Application {
+
+    static final Logger logger = Logger.getLogger(Application.class);
+
 
     public static void main(String[] args) {
 
+        logger.info("Starting avro kafka producer");
+
+        final AvroProducerModule injectorModule = new AvroProducerModule();
+        final Injector injector = Guice.createInjector(injectorModule);
 
     }
 }
---
+++ src/main/java/com/smahjoub/avroproducer/injection/AvroProducerModule.java
@@ -0,0 +1,13 @@
+package com.smahjoub.avroproducer.injection;
+
+import com.google.inject.AbstractModule;
+
+public class AvroProducerModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+
+
+    }
+}
